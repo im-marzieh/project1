4b0d94a4491dce991330d6fda077aa3b0a43fe07
Semicoder <125687040+SemicoderCS@users.noreply.github.com>
2023-07-30 14:32:02 +0330

    first part of project completed

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Typed Js
+var typed = new Typed('#element', {
+    strings: [
+        'ما میخواهیم بهترین مربی زندگی باشیم',
+        'ما میخواهیم بهترین سخنران در آموزش باشیم',
+    ],
+    typeSpeed: 70,
+    backSpeed: 50,
+    loop: true
+});
+
+// Swiper Js
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
